fdc8b5ce9f3b961b8d5bd8f6aa6d24136fa21702
Lesnykh Ilia <lesnykh@waxbilltechnologies.com>
2026-06-15 11:35:21 +0300

    Update PHP version from 8.1 to 8.3.

--- tests/BitmaskTest.php
+++ tests/BitmaskTest.php
@@ -4,26 +4,28 @@ declare(strict_types=1);
 namespace Aliance\Bitmask\Tests;
 
 use Aliance\Bitmask\Bitmask;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 /**
  * Unit tests for simple bitmask implementation.
- * @covers \Aliance\Bitmask\Bitmask
  */
+#[CoversClass(Bitmask::class)]
 class BitmaskTest extends TestCase
 {
     private Bitmask $bitmask;
 
     public function testBitmaskCreation(): void
     {
-        $this->assertInstanceOf(Bitmask::class, new Bitmask());
+        self::assertInstanceOf(Bitmask::class, new Bitmask());
     }
 
     public function testEmptyBitmask(): void
     {
-        $this->assertEquals(0, $this->bitmask->getSetBitsCount());
-        $this->assertEquals(0, $this->bitmask->getMask());
-        $this->assertFalse($this->bitmask->issetBit(1));
+        self::assertEquals(0, $this->bitmask->getSetBitsCount());
+        self::assertEquals(0, $this->bitmask->getMask());
+        self::assertFalse($this->bitmask->issetBit(1));
     }
 
     public function testThatTooBigBitCauseAnException(): void
@@ -34,82 +36,81 @@ class BitmaskTest extends TestCase
 
     public function testMaskSetting(): void
     {
-        $this->assertEquals(0, $this->bitmask->getMask());
+        self::assertEquals(0, $this->bitmask->getMask());
         $this->bitmask->setMask(1024);
-        $this->assertEquals(1024, $this->bitmask->getMask());
+        self::assertEquals(1024, $this->bitmask->getMask());
     }
 
     public function testMaskAdding(): void
     {
-        $this->assertEquals(0, $this->bitmask->getMask());
+        self::assertEquals(0, $this->bitmask->getMask());
 
         // adding 10th bit
         $this->bitmask->addMask(1024);
-        $this->assertEquals(1024, $this->bitmask->getMask());
+        self::assertEquals(1024, $this->bitmask->getMask());
 
         // adding 3rd bit
         $this->bitmask->addMask(8);
-        $this->assertEquals(1032, $this->bitmask->getMask());
+        self::assertEquals(1032, $this->bitmask->getMask());
     }
 
     public function testMaskDeleting(): void
     {
-        $this->assertEquals(0, $this->bitmask->getMask());
+        self::assertEquals(0, $this->bitmask->getMask());
 
         // adding 3rd & 10th bits
         $this->bitmask->setMask(1032);
-        $this->assertEquals(1032, $this->bitmask->getMask());
+        self::assertEquals(1032, $this->bitmask->getMask());
 
         // deleting 10th bit
         $this->bitmask->deleteMask(1024);
-        $this->assertEquals(8, $this->bitmask->getMask());
+        self::assertEquals(8, $this->bitmask->getMask());
     }
 
     public function testBits(): void
     {
-        $this->assertFalse($this->bitmask->issetBit(3));
-        $this->assertFalse($this->bitmask->issetBit(5));
-        $this->assertFalse($this->bitmask->issetBit(12));
-        $this->assertFalse($this->bitmask->issetBit(63));
+        self::assertFalse($this->bitmask->issetBit(3));
+        self::assertFalse($this->bitmask->issetBit(5));
+        self::assertFalse($this->bitmask->issetBit(12));
+        self::assertFalse($this->bitmask->issetBit(63));
 
         $this->bitmask->setBit(3);
         $this->bitmask->setBit(12);
         $this->bitmask->setBit(63);
 
-        $this->assertTrue($this->bitmask->issetBit(3));
-        $this->assertFalse($this->bitmask->issetBit(5));
-        $this->assertTrue($this->bitmask->issetBit(12));
-        $this->assertTrue($this->bitmask->issetBit(63));
+        self::assertTrue($this->bitmask->issetBit(3));
+        self::assertFalse($this->bitmask->issetBit(5));
+        self::assertTrue($this->bitmask->issetBit(12));
+        self::assertTrue($this->bitmask->issetBit(63));
 
         $this->bitmask->unsetBit(12);
         $this->bitmask->unsetBit(63);
 
-        $this->assertTrue($this->bitmask->issetBit(3));
-        $this->assertFalse($this->bitmask->issetBit(5));
-        $this->assertFalse($this->bitmask->issetBit(12));
-        $this->assertFalse($this->bitmask->issetBit(63));
+        self::assertTrue($this->bitmask->issetBit(3));
+        self::assertFalse($this->bitmask->issetBit(5));
+        self::assertFalse($this->bitmask->issetBit(12));
+        self::assertFalse($this->bitmask->issetBit(63));
     }
 
     /**
-     * @dataProvider getBitsWithMaskPairs
      * @param int[] $bits
-     * @param int $expectedMask
      */
+    #[DataProvider('getBitsWithMaskPairs')]
     public function testGeneralUsage(array $bits, int $expectedMask): void
     {
         foreach ($bits as $bit) {
             $this->bitmask->setBit($bit);
         }
 
-        $this->assertCount($this->bitmask->getSetBitsCount(), $bits);
-        $this->assertEquals($expectedMask, $this->bitmask->getMask());
+        self::assertCount($this->bitmask->getSetBitsCount(), $bits);
+        self::assertEquals($expectedMask, $this->bitmask->getMask());
 
         foreach ($bits as $bit) {
-            $this->assertTrue($this->bitmask->issetBit($bit));
+            self::assertTrue($this->bitmask->issetBit($bit));
         }
     }
 
-    public function getBitsWithMaskPairs(): array
+    public static function getBitsWithMaskPairs(): array
     {
         return [
             [
